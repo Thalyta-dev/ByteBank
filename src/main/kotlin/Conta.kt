f853e42faab9af6c85ae54ab668a5072635e57c4
Thalyta-dev <thalytamaelycorrea1718@gmail.com>
2021-06-15 17:43:58 -0300

    Criando metodos para a classe conta

--- src/main/kotlin/Conta.kt
+++ src/main/kotlin/Conta.kt
@@ -1,8 +1,57 @@
-class Conta{
+class Conta {
 
     var titular = ""
 
     var numero = 0
 
     var saldo = 0.0
+
+
+
+    fun depositar(valorDeposito: Double){
+
+        this.saldo = valorDeposito;
+
+        println("Foi depositado $valorDeposito na sua conta, saldo atual ${this.saldo}")
+        return
+
+    }
+
+    fun sacar(valorSaque: Double) {
+
+
+        if (saldo >= valorSaque) {
+
+            this.saldo -= valorSaque;
+
+            println("Foi sacado $valorSaque na sua conta, saldo atual ${this.saldo}")
+            return
+
+        }
+
+        println("Sua conta não tem saldo suficiente, saldo atual ${this.saldo}")
+
+
+
+    }
+
+    fun tranferir(contaTransferencia: Conta, valorTranferencia: Double) {
+
+        if (this.saldo >= valorTranferencia) {
+
+            this.saldo -= valorTranferencia
+
+            contaTransferencia.saldo = contaTransferencia.saldo + valorTranferencia
+
+            println(
+                "Tranferencia no valor de $valorTranferencia foi realizada" +
+                        " para titular ${contaTransferencia.titular}"
+            )
+            return
+        }
+
+        println("Tranferencia não realizada, saldo insuficiente")
+    }
+
 }
+
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,9 +4,25 @@ fun main() {
     conta.titular = "Alex"
     conta.numero = 10
     conta.saldo = 20.0
+
+
+    var conta2 = Conta()
+    conta2.titular = "jose"
+    conta2.numero = 10
+    conta2.saldo = 20.0
+
+
+    conta.sacar(20.0)
+
+    conta.depositar(20.0)
+
+
+    conta.tranferir(conta2, 10.0);
+
+    println("Alex ${conta.saldo}")
+    println("jose ${conta2.saldo}")
+
 }
-
-
 
 
 fun testaCondicoes(saldo: Double) {
