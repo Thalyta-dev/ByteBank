89131ceb60a25180e29147a2c58c903fee0c9d6c
Thalyta-dev <thalytamaelycorrea1718@gmail.com>
2021-06-15 15:12:43 -0300

    Estrutura condicional if

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,24 @@
 fun main() {
-    println("Hello World!")
+
+    var titular = "Thalyta "
+    val numeroConta = 1000
+    var saldo = 0.0
+
+
+    if (saldo>0.0){
+
+        println("Conta positiva")
+
+    }else if (saldo<0.0){
+
+        println("Conta negativa")
+
+    }else{
+
+        println("Conta neutra")
+
+    }
+
+    println("tilular $titular")
+
 }
